e403ac67828477ec93cb1be1df5f2e318b52a4a5
Igor Martayan <igor@martayan.org>
2026-03-03 23:22:34 +0100

    Support parallel execution of multiple tools

--- experiments.py
+++ experiments.py
@@ -1,8 +1,19 @@
 import argparse
 import pathlib
+import sys
+
+from concurrent.futures import ThreadPoolExecutor
+from os import cpu_count
 
 from tools import TOOLS
 from tool import basename, execute
+
+
+def run_tool(tool, timed_out, patterns, reads, **params):
+    if not timed_out[tool.name]:
+        time = tool.run(patterns, reads, **params)
+        timed_out[tool.name] |= time == float("inf")
+        sys.stderr.write("\n")
 
 
 def run(args):
@@ -15,55 +26,103 @@ def run(args):
     k = args.kmer_size
     m = args.minimizer_size
 
-    timeout = {tool.name: False for tool in TOOLS}
-    for n in args.num_patterns:
-        ref_fasta = patterns_dir / f"{basename(reads)}_{n}.fa"
-        ref_txt = ref_fasta.with_suffix(".txt")
-        random_fasta = patterns_dir / f"random_{n}.fa"
-        random_txt = random_fasta.with_suffix(".txt")
-        execute(
-            f"pattern_extract -k {k} -n {n} -r {reads} -f {ref_fasta} -t {ref_txt}",
-            silent=True,
-        )
-        execute(
-            f"pattern_extract -k {k} -n {n} -f {random_fasta} -t {random_txt}",
-            silent=True,
-        )
-        for tool in TOOLS:
-            if not timeout[tool.name]:
-                time = tool.run(
-                    ref_fasta,
-                    reads,
-                    repeat=args.repeat,
-                    timeout=args.timeout,
-                    overwrite_log=args.overwrite_logs,
-                    log_dir=log_dir,
-                    num_patterns=n,
-                    random=False,
-                    threads=args.threads,
-                    threshold=args.threshold,
-                    k=k,
-                    m=m,
-                )
-                timeout[tool.name] |= time == float("inf")
-                print()
-            if not timeout[tool.name]:
-                time = tool.run(
-                    random_fasta,
-                    reads,
-                    repeat=args.repeat,
-                    timeout=args.timeout,
-                    overwrite_log=args.overwrite_logs,
-                    log_dir=log_dir,
-                    num_patterns=n,
-                    random=True,
-                    threads=args.threads,
-                    threshold=args.threshold,
-                    k=k,
-                    m=m,
-                )
-                timeout[tool.name] |= time == float("inf")
-                print()
+    timed_out = {tool.name: False for tool in TOOLS}
+    max_workers = max(args.max_workers // args.threads, 1)
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = []
+        for n in args.num_patterns:
+            ref_fasta = patterns_dir / f"{basename(reads)}_{n}.fa"
+            ref_txt = ref_fasta.with_suffix(".txt")
+            random_fasta = patterns_dir / f"random_{n}.fa"
+            random_txt = random_fasta.with_suffix(".txt")
+            execute(
+                f"pattern_extract -k {k} -n {n} -r {reads} -f {ref_fasta} -t {ref_txt}",
+                silent=True,
+            )
+            execute(
+                f"pattern_extract -k {k} -n {n} -f {random_fasta} -t {random_txt}",
+                silent=True,
+            )
+            for tool in TOOLS:
+                if max_workers > 1:
+                    futures.append(
+                        executor.submit(
+                            run_tool,
+                            tool,
+                            timed_out,
+                            ref_fasta,
+                            reads,
+                            repeat=args.repeat,
+                            timeout=args.timeout,
+                            overwrite_log=args.overwrite_logs,
+                            log_dir=log_dir,
+                            num_patterns=n,
+                            random=False,
+                            threads=args.threads,
+                            threshold=args.threshold,
+                            k=k,
+                            m=m,
+                            silent=True,
+                        )
+                    )
+                else:
+                    run_tool(
+                        tool,
+                        timed_out,
+                        ref_fasta,
+                        reads,
+                        repeat=args.repeat,
+                        timeout=args.timeout,
+                        overwrite_log=args.overwrite_logs,
+                        log_dir=log_dir,
+                        num_patterns=n,
+                        random=False,
+                        threads=args.threads,
+                        threshold=args.threshold,
+                        k=k,
+                        m=m,
+                    )
+            for tool in TOOLS:
+                if max_workers > 1:
+                    futures.append(
+                        executor.submit(
+                            run_tool,
+                            tool,
+                            timed_out,
+                            random_fasta,
+                            reads,
+                            repeat=args.repeat,
+                            timeout=args.timeout,
+                            overwrite_log=args.overwrite_logs,
+                            log_dir=log_dir,
+                            num_patterns=n,
+                            random=True,
+                            threads=args.threads,
+                            threshold=args.threshold,
+                            k=k,
+                            m=m,
+                            silent=True,
+                        )
+                    )
+                else:
+                    run_tool(
+                        tool,
+                        timed_out,
+                        random_fasta,
+                        reads,
+                        repeat=args.repeat,
+                        timeout=args.timeout,
+                        overwrite_log=args.overwrite_logs,
+                        log_dir=log_dir,
+                        num_patterns=n,
+                        random=True,
+                        threads=args.threads,
+                        threshold=args.threshold,
+                        k=k,
+                        m=m,
+                    )
+        for future in futures:
+            future.result()
 
 
 if __name__ == "__main__":
@@ -105,7 +164,18 @@ if __name__ == "__main__":
         default=False,
     )
     parser.add_argument(
-        "-j", "--threads", help="number of threads [8]", type=int, default=8
+        "-j",
+        "--threads",
+        help="number of threads for each tool [8]",
+        type=int,
+        default=8,
+    )
+    parser.add_argument(
+        "-w",
+        "--max_workers",
+        help="total number of workers shared between all tools",
+        type=int,
+        default=cpu_count() // 2,
     )
     parser.add_argument(
         "-t",
