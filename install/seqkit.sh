e2cb5c34c68e35fe3f9fa87a2de544ee2b0583da
Igor Martayan <igor@martayan.org>
2026-03-10 14:59:07 +0100

    Update seqkit installation script

--- install/seqkit.sh
+++ install/seqkit.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -euxo pipefail
 
-if command -v "seqkit" >/dev/null 2>&1; then
+tool="seqkit"
+gh_repo="shenwei356/seqkit"
+
+if command -v ${tool} >/dev/null 2>&1; then
     exit 0
 fi
 
-version=$(curl -s https://api.github.com/repos/shenwei356/seqkit/releases/latest | jq -r '.tag_name')
+version=$(curl -s https://api.github.com/repos/${gh_repo}/releases/latest | jq -r '.tag_name')
 
 # https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script
 unamestr=$(uname)
@@ -31,6 +34,6 @@ fi
 
 mkdir -p bin
 cd bin
-wget -O seqkit.tar.gz https://github.com/shenwei356/seqkit/releases/download/${version}/seqkit_${platform}_${arch}.tar.gz
-tar -xf seqkit.tar.gz
-rm seqkit.tar.gz
+wget -O ${tool}.tar.gz https://github.com/${gh_repo}/releases/download/${version}/${tool}_${platform}_${arch}.tar.gz
+tar -xf ${tool}.tar.gz
+rm ${tool}.tar.gz
